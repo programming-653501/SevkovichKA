e1d35f929fa89aeac2bfe704ba8417b7cd4fc4ed
Kiryl Siaukovich <k.sevkovich@gmail.com>
2017-05-06 21:47:37 +0300

    Update 5.2.25.cpp

--- LabRabota_6/5.2.25.cpp
+++ LabRabota_6/5.2.25.cpp
@@ -1,8 +1,10 @@
 #include "stdafx.h"
 #include <iostream>
 #include <conio.h>
+#include <string>
 #include "TernaryTree.h"
 
+bool isLettersOnly(std::string pString);
 
 int main()
 {
@@ -11,17 +13,43 @@ int main()
 	std::cout << "Input lines that you want to put into ternary tree.\n";
 	std::cout << "Input empty line when done.\n\n";
 
-	std::string temp_string = " ";
+	std::string TempString;
+	std::getline(std::cin, TempString);
 
-	while (temp_string != "")
+	while (TempString != "")
 	{
-		std::getline(std::cin, temp_string);
-		tree.Put(temp_string);
+		if (isLettersOnly(TempString))
+		{
+			tree.Add(TempString);
+		}
+		else
+		{
+			std::cout << "Wrong input, try again: ";
+		}
+
+		std::getline(std::cin, TempString);
 	}
 
 	std::cout << "Tree depth: " << tree.GetDepth();
 
 	_getch();
-    return 0;
+	return 0;
 }
 
+bool isLettersOnly(std::string pString)
+{
+	int StringSize = pString.size();
+
+	for (size_t i = 0; i < StringSize; i++)
+	{
+		bool isUpperCase = (pString[i] >= 'A' && pString[i] <= 'Z');
+		bool isLowerCase = (pString[i] >= 'a' && pString[i] <= 'z');
+
+		if (!isLowerCase && !isUpperCase)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
